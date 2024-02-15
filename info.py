632881d16e1ba418c57b351915c1630f43b2378b
Rhys Crockett <rhysmorgancrockett@gmail.com>
2024-02-15 11:30:18 +0000

    .

--- info.py
+++ info.py
@@ -84,8 +84,8 @@ def aws_backup():
     else:
         client = boto3.client(
             "s3",
-            aws_access_key_id = "AKIA2FYSKELQWHWU37NZ",
-            aws_secret_access_key = "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",
+            aws_access_key_id = 
+            aws_secret_access_key =
         )
         response = client.list_buckets()
         print(response)
